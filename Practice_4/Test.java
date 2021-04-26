d4ea7224c7f824a0861a0d1e35c19f1635c39944
pifchick <70753421+pifchick@users.noreply.github.com>
2021-04-26 13:05:55 +0300

    Delete Practice_4 directory

--- Practice_4/Test.java
+++
@@ -1,9 +0,0 @@
-package Practice_4;
-
-import java.util.concurrent.ExecutionException;
-
-public class Test {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ExecutService executService = new ExecutService(2);
-    }
-}
