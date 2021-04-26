59e4d2124e800824da598e3a3abaf8c7f3ceca28
pifchick <70753421+pifchick@users.noreply.github.com>
2021-04-26 13:17:15 +0300

    Delete Practice_7 directory

--- Practice_7/Composite/Composer.java
+++
@@ -1,9 +0,0 @@
-package Practice_7.Composite;
-
-public interface Composer {
-
-    public Number value();
-    public void add(Composer part);
-    public void remove(Composer part);
-    public Composer getChild(int index);
-}
--- Practice_7/FlyWeight/Primitive.java
+++
@@ -1,5 +0,0 @@
-package Practice_7.FlyWeight;
-
-public interface Primitive {
-    public void wash(Situational context);
-}
