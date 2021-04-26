820561ab14c3564a72c85d2a39bbb5d688b7b6e3
pifchick <70753421+pifchick@users.noreply.github.com>
2021-04-26 13:38:40 +0300

    Update NicaClock.java

--- src/Practice_6/AbstractFactory/NicaClock.java
+++ src/Practice_6/AbstractFactory/NicaClock.java
@@ -2,7 +2,7 @@ package Practice_6.AbstractFactory;
 
 public class NicaClock implements ClockFactory{
     public NicaClock(){
-        System.out.println("Часовая фабрика Ника");
+        System.out.println("Фабрика часов Пейна");
     }
 
     @Override
