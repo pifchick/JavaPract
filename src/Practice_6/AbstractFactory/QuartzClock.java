910d101d2d0069420af3057d18f1b718a86b5837
pifchick <70753421+pifchick@users.noreply.github.com>
2021-04-26 13:44:03 +0300

    Update QuartzClock.java

--- src/Practice_6/AbstractFactory/QuartzClock.java
+++ src/Practice_6/AbstractFactory/QuartzClock.java
@@ -2,6 +2,6 @@ package Practice_6.AbstractFactory;
 
 public class QuartzClock implements Clock {
     public QuartzClock(){
-        System.out.println("Кварцевые часы от часовой фабрики Ника изготовлены");
+        System.out.println("Коллекционные часы от фабрики часов  Пена изготовлены");
     }
 }
